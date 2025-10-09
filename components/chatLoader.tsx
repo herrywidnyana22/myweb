2d3a193d857e3f6c486e9db71872fff267e2c1b9
herrywidnyana22 <115609431+herrywidnyana22@users.noreply.github.com>
2025-10-09 21:09:00 +0800

    Update chatLoader.tsx

--- components/chatLoader.tsx
+++ components/chatLoader.tsx
@@ -1,9 +1,9 @@
 export const ChatLoader = () => {
   return (
     <div className='flex gap-1 mt-2'>
-      <span className='w-2 h-2 bg-gray-400 rounded-full animate-bounce [animation-delay:0ms]'></span>
-      <span className='w-2 h-2 bg-gray-400 rounded-full animate-bounce [animation-delay:150ms]'></span>
-      <span className='w-2 h-2 bg-gray-400 rounded-full animate-bounce [animation-delay:300ms]'></span>
+      <span className='w-2 h-2 bg-gray-400 rounded-full animate-bounce [animation-delay:0ms]'/>
+      <span className='w-2 h-2 bg-gray-400 rounded-full animate-bounce [animation-delay:150ms]'/>
+      <span className='w-2 h-2 bg-gray-400 rounded-full animate-bounce [animation-delay:300ms]'/>
     </div>
   );
 };
