9affc499210013227bcec44cac9291f5fb3053d7
herrywidnyana22 <herrywidnyana@gmail.com>
2025-11-22 15:40:19 +0800

    centering chattelegram notice

--- components/chat/chatNotice.tsx
+++ components/chat/chatNotice.tsx
@@ -5,7 +5,7 @@ export const ChatNotice = () => {
     const { setChatMode, ui } = useApp()
     return ( 
         <div className='flex items-center justify-center flex-col text-white pt-4'>
-            <p className='text-xs font-light'>{ui.telegramNotice}</p>
+            <p className='text-xs font-light text-center'>{ui.telegramNotice}</p>
             <button 
                 onClick={() => setChatMode("default")}
                 className='flex gap-1 items-center justify-center p-2 border rounded-md text-xs mt-2 cursor-pointer bg-gray-600/90 hover:bg-rose-500 hover:font-semibold transition'
