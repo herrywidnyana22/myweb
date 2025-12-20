8170c9ea96f61b364061d6988f76d615a6c0d982
herrywidnyana22 <herrywidnyana@gmail.com>
2025-12-20 21:27:30 +0800

    change some data

--- components/home/overlay.tsx
+++ components/home/overlay.tsx
@@ -1,10 +1,10 @@
 import { useAppStore } from "@/store/app";
 
 export const Overlay = () => {
-    const { isInputFocused } = useAppStore();
+    const { isInputFocused, isMinimized } = useAppStore();
     return ( 
         <>
-            {isInputFocused && (
+            {(isInputFocused || !isMinimized) && (
                 <div className="absolute inset-0 z-50 backdrop-blur-sm bg-black/30 transition-opacity" />
             )}
         </>
