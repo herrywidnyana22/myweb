08ff52e43df1d74fc7a73a32b23469662aeda216
herrywidnyana22 <herrywidnyana@gmail.com>
2026-03-08 17:38:14 +0800

    change setup

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,23 +3,13 @@ import { PrismaPg } from '@prisma/adapter-pg';
 
 const connectionString = process.env.DATABASE_URL;
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const adapter = new PrismaPg({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
-
-// const adapter = new PrismaPg({ connectionString });
-
-// const prisma =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-//     adapter,
-//   })
-
-// if (process.env.NODE_ENV !== 'production') {
-//   globalForPrisma.prisma = prisma
-// }
 
 export default prisma;
