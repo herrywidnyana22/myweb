3fe3802669785cba6cc8df9efa1456626c85f9d1
herrywidnyana22 <herrywidnyana@gmail.com>
2026-03-08 14:41:07 +0800

    fix some bug

--- prisma/migrations/20260105140109_adjust_multilingual_fields/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `description` column on the `Experience` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Experience" DROP COLUMN "description",
-ADD COLUMN     "description" JSONB;
